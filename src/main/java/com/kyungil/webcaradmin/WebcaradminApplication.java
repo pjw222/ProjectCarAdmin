2c0fc246d47a865910dacbab030abc6fd6f082ae
pjw222 <wpzktm@naver.com>
2023-12-11 14:19:02 +0900

    -commit

---
+++ src/main/java/com/kyungil/webcaradmin/WebcaradminApplication.java
@@ -0,0 +1,13 @@
+package com.kyungil.webcaradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcaradminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcaradminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyungil/webcaradmin/WebcaradminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungil.webcaradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcaradminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
